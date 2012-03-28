986e5a5df4dcda3d7b1944b7bf4751dcb961d373
Alex Clark <aclark@aclark.net>
2012-03-28 16:22:22 -0400

    Add files

---
+++ src/eea_custom/eea_custom/__init__.py
@@ -0,0 +1,13 @@
+from Products.Five.browser.pagetemplatefile import \
+    ViewPageTemplateFile
+from zope.publisher.browser import BrowserPage
+
+class EEACustomView(BrowserPage):
+    """
+    EEA custom view
+    """
+
+    template = ViewPageTemplateFile('eea_custom.pt')
+
+    def __call__(self):
+        return self.template()
